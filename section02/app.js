699b95121bce2ec7046a8b980770523847f22835
Jarkko Aalto <jarkko.aalto@live.fi>
2018-10-16 20:34:09 +0300

    adding app.js file to parsing different currensies www-page and adding html file populating currencies to select lists

---
+++ section02/app.js
@@ -0,0 +1,37 @@
+new Vue({
+    el: '#app',
+
+    data: {
+        currencies: {}
+    },
+
+    mounted(){
+        this.getCurrencies();
+    },
+
+    computed: {
+    formattedCurrensies(){
+        return Object.values(this.currencies)
+        },
+    },
+
+    methods: {
+        getCurrencies(){
+
+            const currencies = localStorage.getItem('currencies')
+            if (currencies){
+                this.currencies = JSON.parse(currencies);
+
+                return;
+            }
+
+            axios.get(' https://free.currencyconverterapi.com/api/v6/currencies')
+            .then(response => {
+                    this.currencies = response.data.results;
+                    localStorage.setItem('currencies', JSON.stringify(response.data.results))
+    
+            });
+        }    
+    }
+   
+})
