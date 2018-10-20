2a7b780bd588919f0fb4484eaf69ae566c685e15
Jarkko Aalto <jarkko.aalto@live.fi>
2018-10-20 15:53:00 +0300

    upgrading vue-practice-shop

---
+++ section04/vue-practice-shop/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+ 
+ <div class="container">
+      <div class="row">
+            <div class="col-md-7">
+            
+            </div>
+            <div class="col-md-5">
+            
+            </div>
+      </div>  
+ </div>
+</template>
+
+<script>
+
+
+})
+
+
+export default {
+  name: 'app',
+ 
+
+}
+</script>
+
+<style>
+  body {
+    background-color: #FBF8F3;
+  }
+</style>
